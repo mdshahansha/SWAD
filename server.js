283c888ee9a23b5f21fcf375045bf6fbfd3679cd
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-06-14 05:59:45 +0530

    added

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();//ise app object ke andar sab function  rhega jo hame express deti h
+
+const ejs=require('ejs');
+const path=require('path');
+const expressLayout=require('express-ejs-layouts');
+
+const PORT= 3000;//ye env wala tab  hoga jab hamara production ke liye hoga  || process.env.PORT ||
+
+
+app.get('/', (req,res)=>{
+    res.render('home');
+})
+
+//set Templates engine 
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+
+
+
+app.listen(PORT,()=>{
+    console.log(`listening on port 3000  to ${PORT}`);
+})
