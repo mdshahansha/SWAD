dc7f2ae9b061f43a158e69c0c105efa37fa76fe5
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-06-16 08:10:33 +0530

    database setup

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const path=require('path');
 const expressLayout=require('express-ejs-layouts');
 
 const PORT= 3000;//ye env wala tab  hoga jab hamara production ke liye hoga  || process.env.PORT ||
+const mongoose=require('mongoose');
+
+//database connection 
+const url='mongodb://localhost/pizza-test';
+mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database connceted...');
+}).catch(err=>{
+    console.log('Connection failed....');
+})
+
 
 app.use(express.static('public'));
 
