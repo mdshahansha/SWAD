c2297f84b0eb9fcaae4019cff8c97b8b4aadb80f
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-06-18 12:25:10 +0530

    order admin handle

--- app/http/controllers/admin/orderController.js
+++ app/http/controllers/admin/orderController.js
@@ -1,7 +1,12 @@
+const order = require("../../../models/order")
+//ne means notequal
 function orderController(){
     return {
         index(req,res){
-            
+            order.find({status:{$ne:'ompleted'} }, null , {sort:{'createdAt':-1}}).
+            populate('customerId','-password').exec((err,order)=>{
+                res.render('admin/orders')
+            })
         }
     }
 }
